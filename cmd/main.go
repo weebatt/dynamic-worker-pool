e31225650f2bce137eab1aec6bffd1dd86ef2a45
weebatt <408116@niuitmo.ru>
2025-06-10 21:58:17 +0300

    feat: add final version

--- cmd/main.go
+++ cmd/main.go
@@ -2,18 +2,38 @@ package main
 
 import (
 	"dynamic-worker-pool/internal/dispatcher"
-	"dynamic-worker-pool/internal/worker"
+	"fmt"
 	"time"
 )
 
 func main() {
-	maxWorkers := 3
-	dispatcher := dispatcher.NewDispatcher(maxWorkers)
-	dispatcher.Run()
+	d := dispatcher.NewDispatcher(2)
 
+	// Send initial jobs
 	for i := 1; i <= 5; i++ {
-		job := worker.Job{ID: i, Payload: "Job payload"}
-		dispatcher.Dispatch(job)
+		d.Dispatch(fmt.Sprintf("Job %d payload", i))
+	}
+
+	time.Sleep(1 * time.Second)
+
+	// Dynamically add a worker
+	fmt.Println("Adding a worker...")
+	d.AddWorker()
+
+	// Send more jobs
+	for i := 6; i <= 8; i++ {
+		d.Dispatch(fmt.Sprintf("Job %d payload", i))
+	}
+
+	time.Sleep(1 * time.Second)
+
+	// Dynamically remove a worker
+	fmt.Println("Removing a worker...")
+	d.RemoveWorker()
+
+	// Send more jobs
+	for i := 9; i <= 10; i++ {
+		d.Dispatch(fmt.Sprintf("Job %d payload", i))
 	}
 
 	time.Sleep(2 * time.Second)
